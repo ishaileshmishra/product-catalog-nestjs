322270485c638a9b5949bcbe302c203f9630a98d
mshaileshr@gmail.com <Yes@383840>
2020-07-22 15:32:14 +0530

    config integrated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,14 @@
-// Module is a class annotated with a @Module() decorator.
-// The @Module() decorator provides metadata that Nest makes use of to organize the application structure.
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { config } from './config';
+import config from './config/keys';
 
-// Add your array of modules in the section of impots.
 @Module({
   imports: [
     ProductsModule,
-    // get the root link from https://cloud.mongodb.com/
-    // find the  username and password from the secuirity section and Network Access Tab
-    // Add process.env.MONGO_URI from .env file
-    MongooseModule.forRoot(
-      'mongodb+srv://shaileshmishra:VEvKCG4zYt9r7dfX@cluster0-lmmr1.mongodb.net/product-catalog-nestjs?retryWrites=true&w=majority',
-      //process.env.MONGO_URI,
-    ),
+    MongooseModule.forRoot(config.mongoURI),
     AuthModule,
-    ConfigModule.forRoot({
-      // envFilePath: '.development.env', // Custom env file path
-      // ignoreEnvFile: true, // Disable env variables loading
-      isGlobal: true, // Use module globally
-      load: [config],
-    }),
   ],
 })
 export class AppModule {}
